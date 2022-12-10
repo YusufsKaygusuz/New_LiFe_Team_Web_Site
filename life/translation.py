d21cab3caff073c32e7cc8f45da1b26e091b6ee7
Burak Can Akıncı <ayatalzidi2000@gmail.com>
2022-12-10 21:40:41 +0300

    modelsPage

--- life/translation.py
+++
@@ -1,7 +0,0 @@
-from life.models import NewLifeManagementMember
-from modeltranslation.translator import TranslationOptions,register
-
-
-@register(NewLifeManagementMember)
-class NewLifeManagementMemberTranslationOptions(TranslationOptions):
-    fields = ('title',)
