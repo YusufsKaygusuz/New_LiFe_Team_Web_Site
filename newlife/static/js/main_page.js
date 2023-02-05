809463a6967aeb3757e48d2d84542fe5c90bcefb
Yusuf S. Kaygusuz <yusufskaygusuz@gmail.com>
2023-02-05 12:49:00 +0300

    Arranged for Clean Code

--- newlife/static/js/main_page.js
+++ newlife/static/js/main_page.js
@@ -2,22 +2,8 @@ let navbar = document.querySelector('.navbar');
 
     document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-     //searchForm.classList.remove('active');
-     cartItem.classList.remove('active');
 }
-/*
-let searchForm = document.querySelector('.search-form');
-
-    document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-     navbar.classList.remove('active');
-     cartItem.classList.remove('active');
-}
-*/
-
 
 window.onscroll = () =>{
     navbar.classList.remove('active');
-     //searchForm.classList.remove('active');
-     cartItem.classList.remove('active');
 }
