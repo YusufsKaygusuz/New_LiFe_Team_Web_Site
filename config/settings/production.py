0d592f15252ec89075af436163d4feff18aafc89
brktrlw <brktrl@protonmail.ch>
2023-02-15 15:02:14 +0300

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,12 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[
+    "127.0.0.1",
+    "localhost",
+    "django",
+    "https://newlifewebsite.herokuapp.com/",
+])
 
 # DATABASES
 # ------------------------------------------------------------------------------
